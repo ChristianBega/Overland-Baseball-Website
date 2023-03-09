4912537f80be535000b19d3fd6811ad347f844be
Chris <cbega1227@gmail.com>
2023-03-08 20:17:57 -0700

    Completed routing to pages using react-router-dom

--- src/components/framerMotion/AnimationRoutes.component.jsx
+++ src/components/framerMotion/AnimationRoutes.component.jsx
@@ -2,14 +2,22 @@ import React from "react";
 
 import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+import HomePage from "../../pages/home.page";
+import BoostersPage from "../../pages/boosters.page";
+import EventsPage from "../../pages/events.page";
+import DocumentsPage from "../../pages/documents.page";
+import RoosterPage from "../../pages/rooster.page";
 
 export default function AnimationRoutes() {
   const location = useLocation();
   return (
     <AnimatePresence initial={true} mode="wait">
       <Routes key={location.pathname} location={location}>
-        
-        <Route path="/" element={""}></Route>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/boosters" element={<BoostersPage />}></Route>
+        <Route path="/events" element={<EventsPage />}></Route>
+        <Route path="/rooster" element={<RoosterPage />}></Route>
+        <Route path="/documents" element={<DocumentsPage />}></Route>
       </Routes>
     </AnimatePresence>
   );
