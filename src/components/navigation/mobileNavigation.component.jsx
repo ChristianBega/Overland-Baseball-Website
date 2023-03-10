2c8935ec2901615bb3e46324a321c94c71eeb5b2
Chris <cbega1227@gmail.com>
2023-03-09 18:33:01 -0700

    Working on navigation menu.

---
+++ src/components/navigation/mobileNavigation.component.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+// Icons
+import MenuIcon from "@mui/icons-material/Menu";
+
+// Mui components
+import { AppBar, Box, Toolbar, Typography, IconButton, Button, Drawer, styled } from "@mui/material";
+import NavigationListItems from "./navigationListItems.component";
+
+// const StyledNavigation = styled(AppBar)(({ theme }) => ({}));
+
+const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-between",
+  textAlign: "center",
+  // spacing: theme.spacing(19),
+  padding: theme.spacing(0, 3.5), // 0px 14px
+  color: theme.palette.text.primary,
+  background: theme.palette.primary.main,
+}));
+
+const StyledDrawerMenu = styled(Drawer)(({ theme }) => ({
+  background: "rgba(0, 0, 0, 0.7)",
+  width: "100%",
+  [theme.breakpoints.up("md")]: {
+    width: "50%",
+  },
+  // zIndex: "-1",
+}));
+
+export default function MobileNavigation() {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const handleOpen = () => {
+    setOpenMenu(true);
+
+    // setAnchorEl(null);
+  };
+  const handleClose = () => {
+    setOpenMenu(false);
+    // setAnchorEl(null);
+  };
+
+  return (
+    <Box component="header" className="main-header" sx={{ flexGrow: 1 }}>
+      <AppBar component="nav" position="static">
+        <StyledToolbar>
+          <IconButton onClick={handleOpen} size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <StyledDrawerMenu open={openMenu} anchor={"left"} onClose={handleClose}>
+            <NavigationListItems setOpenMenu={setOpenMenu} openMenu={openMenu} handleClose={handleClose} />
+          </StyledDrawerMenu>
+
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Overland Baseball
+          </Typography>
+          <Button size="box">Login</Button>
+        </StyledToolbar>
+      </AppBar>
+    </Box>
+  );
+}
---
+++ src/components/navigation/navigationListItems.component.jsx
@@ -0,0 +1,53 @@
+import { IconButton, List, ListItem, styled } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+
+// import { Link } from "react-router-dom";
+
+// Navigation menu item data
+const menuItemData = [
+  { linkName: "Home", urlPath: "/" },
+  { linkName: "Team Rooster", urlPath: "/rooster" },
+  { linkName: "Events", urlPath: "/events" },
+  { linkName: "Boosters", urlPath: "/boosters" },
+  { linkName: "Documents", urlPath: "/documents" },
+  { linkName: "Alumni", urlPath: "/" },
+  { linkName: "Sponsors", urlPath: "/" },
+];
+
+// Styled Components
+const StyledList = styled(List)(({ theme }) => ({
+  color: theme.palette.primary.main,
+  display: "flex",
+  alignItems: "center",
+  flexDirection: "column",
+  rowGap: theme.spacing(4), // 16px
+  minWidth: "100vw",
+  [theme.breakpoints.up("md")]: {
+    minWidth: "50vw",
+  },
+}));
+const StyledListItem = styled(ListItem)(({ theme }) => ({
+  width: "80%",
+  border: "1px solid red",
+}));
+
+// Get menu items
+const getMenuItems = () => (
+  <StyledList>
+    {menuItemData.map((menuItem, index) => (
+      <StyledListItem key={menuItem.linkName}>{menuItem.linkName}</StyledListItem>
+    ))}
+  </StyledList>
+);
+
+export default function NavigationListItems({ handleClose }) {
+  // const [menuData, setMenuData] = useState(menuItemData);
+  return (
+    <>
+      <IconButton onClick={handleClose} color="primary.main" aria-label="exit menu">
+        <CloseIcon />
+      </IconButton>
+      {getMenuItems()}
+    </>
+  );
+}
