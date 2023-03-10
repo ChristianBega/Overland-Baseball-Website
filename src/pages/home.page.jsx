e6276e39bdbb1c182230f0a53cfafae83c648bed
Chris <cbega1227@gmail.com>
2023-03-09 22:17:00 -0700

    Cleaned up code to deploy to netlify

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import { Box, Button, styled } from "@mui/material";
 
-const StyledBox = styled(Box)(({ theme }) => ({
-  backgroundColor: theme.palette.primary.main,
-  color: theme.palette.secondary.main,
-}));
 export default function HomePage() {
   // const theme = useTheme();
   return <div>Home Page</div>;
