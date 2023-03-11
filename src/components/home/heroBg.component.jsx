84d8d2684565d1665813d08a3f01eb41cdb34561
Chris <cbega1227@gmail.com>
2023-03-10 19:56:11 -0700

    Moved hero text.

--- src/components/home/heroBg.component.jsx
+++ src/components/home/heroBg.component.jsx
@@ -1,15 +1,32 @@
-import { Box } from "@mui/material";
+import styled from "@emotion/styled";
+import { Box, Paper, Typography } from "@mui/material";
 import React from "react";
 
 import HeroImage from "../../assets/heroImg.jpg";
+const StyledPaper = styled(Paper)(({ theme }) => ({
+  // display: "flex",
+  // justifyContent: "space-between",
+  margin: theme.spacing(8),
+  textAlign: "center",
+  padding: theme.spacing(6), // 14px
+  color: theme.palette.text.primary,
+  background: theme.palette.accent.accentOne,
+}));
 export default function HeroBackground() {
   return (
-    <section id="hero-background" style={{ position: "relative" }}>
+    <section id="hero-background">
       <Box
         component="img"
         src={HeroImage}
-        sx={{ width: "100vw", height: "100vh", objectFit: "cover", position: "absolute", top: 0, bottom: 0, zIndex: -1 }}
+        sx={{ width: "100vw", height: "100vh", objectFit: "cover", position: "absolute", top: 0, bottom: 0, zIndex: -1, mt: 15 }}
       ></Box>
+      <StyledPaper>
+        <Typography typography="h1">Overland Baseball</Typography>
+        <Typography typography="bodyTextLg" mt={4}>
+          Dignissimos et possimus autem in aspernatur possimus id expedita atque. Ut galisum nostrum in galisum omnis sit voluptatem ipsa sit enim
+          cumque et voluptatem facilis!
+        </Typography>
+      </StyledPaper>
     </section>
   );
 }
--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,30 +1,14 @@
 import React from "react";
 import HeroBackground from "../components/home/heroBg.component";
-import { Container, styled, Typography } from "@mui/material";
+import { Container, Grid, styled, Typography } from "@mui/material";
 import { Paper } from "@mui/material";
-const StyledPaper = styled(Paper)(({ theme }) => ({
-  // display: "flex",
-  // justifyContent: "space-between",
-  marginTop: theme.spacing(5),
-  textAlign: "center",
-  padding: theme.spacing(6), // 14px
-  color: theme.palette.text.primary,
-  background: theme.palette.accent.accentOne,
-}));
+
 export default function HomePage() {
   // const theme = useTheme();
   return (
     <>
       <HeroBackground />
-      <Container maxWidth="lg">
-        {/* <StyledPaper>
-          <Typography typography="h1">Overland Baseball</Typography>
-          <Typography typography="bodyTextLg" mt={4}>
-            Dignissimos et possimus autem in aspernatur possimus id expedita atque. Ut galisum nostrum in galisum omnis sit voluptatem ipsa sit enim
-            cumque et voluptatem facilis!
-          </Typography>
-        </StyledPaper> */}
-      </Container>
+      <Grid container id="h" component="section" maxWidth="lg"></Grid>
     </>
   );
 }
