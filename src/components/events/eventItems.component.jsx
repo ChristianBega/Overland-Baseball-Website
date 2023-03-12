13a6bb093a6b960ee777742a6365d53a704bf74e
Chris <cbega1227@gmail.com>
2023-03-12 16:33:52 -0600

    built out basic events component

--- src/components/events/eventItems.component.jsx
+++ src/components/events/eventItems.component.jsx
@@ -1,10 +1,19 @@
 import { ListItem, ListItemText } from "@mui/material";
 import React from "react";
 
-export default function EventItems() {
+export default function EventItems({ event }) {
+  // extra data from event object : description, extraInformation
+  const { eventName, location, date, time } = event;
+
   return (
-    <ListItem>
-      <ListItemText primary="Testing" />
+    <ListItem sx={{ maxWidth: "1000px", margin: "auto" }}>
+      <ListItemText sx={{ flex: 2 }} primary={eventName} />
+      <ListItemText sx={{ flex: 2 }} primary={location} />
+      <ListItemText
+        sx={{ flex: 0.5, display: "flex", alignItems: "center", justifyContent: "flex-start", columnGap: 2 }}
+        primary={date}
+        secondary={time}
+      />
     </ListItem>
   );
 }
