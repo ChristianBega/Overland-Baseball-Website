c213226ebcb47c40d6acfb26e6b4faed502c9829
Chris B <cbega1227@gmail.com>
2023-03-16 21:50:37 -0400

    Fixed up documents page.

--- src/pages/documents.page.jsx
+++ src/pages/documents.page.jsx
@@ -1,11 +1,12 @@
-import { Grid } from "@mui/material";
+import { Grid, useTheme } from "@mui/material";
 import React from "react";
 import DocumentsGrid from "../components/documents/documentGrid.component";
 
 export default function DocumentsPage() {
+  const theme = useTheme()
   return (
-    <section id="documents-section" style={{ display: "flex", justifyContent: " center" }}>
-      <Grid container maxWidth="lg" spacing={{ xs: 4, md: 6 }}>
+    <section id="documents-section" style={{ display: "flex", justifyContent: " center", marginBlock : theme.spacing(5) }}>
+      <Grid id="main-grid" container maxWidth="xl" spacing={{ xs: 4, md: 6 }}>
         <DocumentsGrid />
         <DocumentsGrid />
         <DocumentsGrid />
