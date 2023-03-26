36c5af0e959d27badb56e9edf98a8fa247db380d
Chris B <cbega1227@gmail.com>
2023-03-26 11:20:35 -0400

    Refactored fundraisers component. Added images to fundraiser data.

--- client/src/components/events/fundraisers.component.jsx
+++ client/src/components/events/fundraisers.component.jsx
@@ -1,30 +1,68 @@
-import { Typography } from "@mui/material";
-import Grid from "@mui/material/Grid/Grid";
-import { Box } from "@mui/system";
 import React from "react";
+import Grid from "@mui/material/Grid/Grid";
+import { Typography, Box } from "@mui/material";
+
+import TournamentsImage from "../../assets/AmericanLegionTournament.jpeg";
+import BroncosImage from "../../assets/broncosVolunteer.png";
+import FieldCleanUpImage from "../../assets/fieldCleanUp.jpg";
+import { styled } from "@mui/system";
+
+// Styled components
+const StyledImageBox = styled(Box)(({ theme }) => ({
+  // display: "none",
+  // justifyContent: "space-between",
+  // textAlign: "center",
+  // padding: theme.spacing(4),
+  // color: theme.palette.text.primary,
+  // background: theme.palette.primary.main,
+  // [theme.breakpoints.up("lg")]: {
+  //   display: "flex",
+  // },
+  objectFit: "cover",
+  height: "100%",
+  width: "100%",
+  maxHeight: "350px",
+  // boxShadow: 10,
+  // alignItems: "stretch",
+  "&:hover": {
+    // backgroundColor: "#f00",
+    cursor: "pointer",
+    border: `2px solid ${theme.palette.secondary.main}`,
+    scale: "1.05",
+    // height : "50px"
+  },
+  
+}));
 
 const fundraisersData = [
   {
     id: "Name 1",
-    content: "box 1",
+    content: "Broncos Games ",
+    image: BroncosImage,
+
     date: "1-12-23",
     time: "11 am",
   },
   {
     id: "Name 2",
-    content: "box 2",
+    content: "Overland tournaments",
+    image: TournamentsImage,
     date: "2-12-23",
     time: "11 am",
   },
   {
     id: "Name 3",
-    content: "box 3",
+    content: "Fall Clean Up",
+    image: FieldCleanUpImage,
+
     date: "9-12-23",
     time: "1 pm",
   },
   {
     id: "Name 4",
     content: "box 4",
+    image: TournamentsImage,
+
     date: "8-22-23",
     time: "8 pm",
   },
@@ -38,8 +76,10 @@ export default function Fundraisers() {
       </Typography>
       <Grid container maxWidth="lg" spacing={4}>
         {fundraisersData.map((fundraiser) => (
-          <Grid item key={fundraiser.id} xs={12} md={6}>
-            <Box sx={{ minHeight: { xs: "200px", lg: "250px" }, backgroundColor: "red" }}>{fundraiser.content}</Box>
+          <Grid item key={fundraiser.id} xs={6} md={3}>
+            <StyledImageBox component="img" boxShadow={10} src={fundraiser.image}>
+              {/* {fundraiser.content} */}
+            </StyledImageBox>
           </Grid>
         ))}
       </Grid>
