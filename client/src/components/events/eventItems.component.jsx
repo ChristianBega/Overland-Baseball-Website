fc500f973680cb0bb125bf6dc2a2779ad5a7264a
Chris B <cbega1227@gmail.com>
2023-03-29 21:13:31 -0400

    Added useMediaQuery to conditonally render schedule item for mobile.

--- client/src/components/events/eventItems.component.jsx
+++ client/src/components/events/eventItems.component.jsx
@@ -1,23 +1,34 @@
 import { useTheme } from "@emotion/react";
+import styled from "@emotion/styled";
 import { TableCell, TableRow, Typography } from "@mui/material";
 import React from "react";
 
-export default function EventItems({ event }) {
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-child(even)": {
+    backgroundColor: "#f2f2f2",
+    boxShadow: 10,
+  },
+}));
+
+export default function EventItems({ event, isMobile }) {
   // extra data from event object : description, extraInformation
   const { eventName, location, date, time } = event;
   const theme = useTheme();
+  // const [isMobile, setIsMobile] = useState();
+
+  // const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  // const checkIsMobile = () => {
+  //   if () {
+  //     alert("yes");
+  //   }
+  // };
 
   return (
-    <TableRow>
-      <TableCell
-        sx={{ textAlign: "center", fontWeight: "bold", backgroundColor: theme.palette.primary.main, color: theme.palette.text.primary, width: "15%" }}
-      >
-        {eventName}
-      </TableCell>
-      <TableCell sx={{ width: "70%" }}>{location}</TableCell>
+    <StyledTableRow>
       <TableCell
         sx={{
-          width: "15%",
+          width: "30%",
           fontWeight: "bold",
           textAlign: "center",
           backgroundColor: theme.palette.secondary.main,
@@ -29,6 +40,22 @@ export default function EventItems({ event }) {
           {time}
         </Typography>
       </TableCell>
-    </TableRow>
+
+      <TableCell sx={{ width: "60%" }}>{location}</TableCell>
+
+      {!isMobile && (
+        <TableCell
+          sx={{
+            textAlign: "center",
+            fontWeight: "bold",
+            backgroundColor: theme.palette.primary.light,
+            color: theme.palette.text.primary,
+            width: "10%",
+          }}
+        >
+          {eventName}
+        </TableCell>
+      )}
+    </StyledTableRow>
   );
 }
--- client/src/components/events/eventsDataGrid.component.jsx
+++ client/src/components/events/eventsDataGrid.component.jsx
@@ -1,19 +1,57 @@
-import { Grid, Paper, Table, TableContainer, Typography } from "@mui/material";
+import { Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, useMediaQuery } from "@mui/material";
 import React from "react";
 import EventItems from "./eventItems.component";
 import { broncoData } from "../../websiteData/events.data";
+import { useTheme } from "@emotion/react";
 
 export default function Events() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
     <Grid item xs={12}>
       <Typography typography="h2" sx={{ textAlign: "center", my: 10 }}>
         Upcoming Events!
       </Typography>
       <TableContainer component={Paper} sx={{ color: "#000", my: 6, maxHeight: 440 }}>
-        <Table aria-label="simple table">
-          {broncoData.map((event, index) => (
-            <EventItems key={index} event={event} />
-          ))}
+        <Table stickyHeader aria-label="simple table">
+          <TableHead sx={{ border: "2px solid red" }}>
+            <TableRow>
+              <TableCell
+                sx={{
+                  textAlign: "center",
+                  fontWeight: "bold",
+                  backgroundColor: theme.palette.primary.main,
+                  color: theme.palette.text.primary,
+                  width: "15%",
+                }}
+              >
+                <Typography typography="h5">Date</Typography>
+              </TableCell>
+
+              <TableCell sx={{ width: "70%", backgroundColor: theme.palette.primary.main, color: theme.palette.text.primary, textAlign: "center" }}>
+                <Typography typography="h5">Location</Typography>
+              </TableCell>
+              {!isMobile && (
+                <TableCell
+                  sx={{
+                    width: "15%",
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    backgroundColor: theme.palette.primary.main,
+                    color: theme.palette.text.primary,
+                  }}
+                >
+                  <Typography typography="h5">Event</Typography>
+                </TableCell>
+              )}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {broncoData.map((event, index) => (
+              <EventItems isMobile={isMobile} key={index} event={event} />
+            ))}
+          </TableBody>
         </Table>
       </TableContainer>
     </Grid>
