acc5624a559577549546aee435a1e4fc702ea051
Chris B <cbega1227@gmail.com>
2023-04-03 13:30:57 -0400

    Added document card styling and data.

--- client/src/components/documents/documentCard.component.jsx
+++ client/src/components/documents/documentCard.component.jsx
@@ -1,12 +1,30 @@
-import { Grid, Typography } from "@mui/material";
 import React from "react";
+import { Card, CardActions, CardContent, Grid, Link, Typography } from "@mui/material";
+import { useTheme } from "@emotion/react";
 
 export default function DocumentCard({ data }) {
+  const theme = useTheme();
   return (
-    <Grid id="document-grid-item" item xs={6} sx={{ textAlign: "center", minHeight: "248px", width : "100%" }}>
-      {/* <Typography typography="h4">{data.documentType}</Typography> */}
-
-      <Typography typography="bodyTextSm">{data.documentName}</Typography>
+    <Grid id="document-grid-item" item xs={12} sm={6}>
+      <Card sx={{ textAlign: "center", minHeight: "260px", width: "100%", boxShadow: 10 }}>
+        <CardContent>
+          <Typography variant="h5" color={theme.palette.primary.main}>
+            {data.documentName}
+          </Typography>
+        </CardContent>
+        <CardActions sx={{ display: "flex", flexDirection: "column", gap: 4, mt: 5 }}>
+          <Link href={data.documentLink} target="_blank" rel="noopener noreferrer" sx={{ height: "100%", width: "100%", textDecoration: "none" }}>
+            <Typography typography={{ xs: "bodyTextSm", md: "bodyTextLg" }} color={theme.palette.secondary.main}>
+              Read more
+            </Typography>
+          </Link>
+          {/* <Button sx={{ maxWidth: "50%", margin: "auto", p: 0 }}>
+            <a id="download" href={data.documentLink} download={data.documentLink}>
+              Download Resume
+            </a>
+          </Button> */}
+        </CardActions>
+      </Card>
     </Grid>
   );
 }
