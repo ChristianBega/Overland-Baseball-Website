98da51f1dbfcba0fc16ddddb5d84c077fa59150c
Chris <cbega1227@gmail.com>
2023-08-20 14:02:01 -0600

    Updated data for roster page and documents data.

--- client/src/websiteData/documents/documents.data.js
+++ client/src/websiteData/documents/documents.data.js
@@ -1,10 +1,10 @@
 // const codeOfConductDoc = "./codeOfConduct.pdf";
 
 export const playerDocuments = [
-  {
-    documentName: "Player & Parent Agreement",
-    documentLink: "https://drive.google.com/file/d/1ev0JsHk2Fbo9jcDfh94cNgZV5MQFeJ3C/view?usp=sharing",
-  },
+  // {
+  //   documentName: "Player & Parent Agreement",
+  //   documentLink: "https://drive.google.com/file/d/1ev0JsHk2Fbo9jcDfh94cNgZV5MQFeJ3C/view?usp=sharing",
+  // },
   {
     documentName: "Player Code of Conduct",
     documentLink: "https://drive.google.com/file/d/1uDXUHmtc32YdsmZGcWbP_0f_H8NjzTV4/view?usp=sharing",
