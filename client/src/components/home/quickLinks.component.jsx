ddbeba7b9dc9d58aeff76fd476e5f70a9fd92992
Chris <cbega1227@gmail.com>
2023-09-17 20:28:16 -0600

    Refactored quick links to use flex box vs grid for more flexibility.

--- client/src/components/home/quickLinks.component.jsx
+++ client/src/components/home/quickLinks.component.jsx
@@ -13,9 +13,7 @@ const StyledPaper = styled(Paper)(({ theme }) => ({
   background: theme.palette.accent.accentOne,
   border: `1px solid ${theme.palette.borders.primary}`,
   "&:hover": {
-    // border: "1px solid red",
     transform: "scale(1.05)",
-    // boxShadow: 50,
     boxShadow: `0px 0px 12px 1px ${theme.palette.secondary.light}`,
   },
 }));
@@ -49,24 +47,22 @@ export default function QuickLinksGrid() {
       <Typography typography="h2" component="h2" sx={{ textAlign: "center", color: theme.palette.primary.main, mb: 10 }}>
         Quick Links
       </Typography>
-      <Grid container spacing={4} sx={{ justifyContent: "center" }}>
+      <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center", gap: { xs: ".5rem", lg: "1rem" } }}>
         {ctaData.map((cta) => (
-          <Grid key={cta.ctaName} item xs={6} sm={5} md={3} lg={3} sx={{ display: "flex", justifyContent: "center" }}>
-            <Link sx={{ display: "flex", justifyContent: "center", maxWidth: "90%" }} href={cta.urlLink} target="_blank" rel="noopener noreferrer">
-              <StyledPaper
-                sx={{
-                  height: { xs: "185px", sm: "300px", g: "350px" },
-                  width: { xs: "185px", sm: "300px", md: "80%", lg: "350px" },
-                  background: "transparent",
-                  boxShadow: 10,
-                }}
-              >
-                <Box component="img" src={cta.ctaImage} sx={{ objectFit: "contain", width: "100%", height: "100%", color: "#c3c1c1a0" }}></Box>
-              </StyledPaper>
-            </Link>
-          </Grid>
+          <Link sx={{ display: "flex", justifyContent: "center", maxWidth: "90%" }} href={cta.urlLink} target="_blank" rel="noopener noreferrer">
+            <StyledPaper
+              sx={{
+                height: { xs: "175px", sm: "200px", lg: "220px" },
+                width: { xs: "175px", sm: "200px", lg: "220px" },
+                background: "transparent",
+                boxShadow: 10,
+              }}
+            >
+              <Box component="img" src={cta.ctaImage} sx={{ objectFit: "contain", width: "100%", height: "100%", color: "#c3c1c1a0" }}></Box>
+            </StyledPaper>
+          </Link>
         ))}
-      </Grid>
+      </Box>
     </Grid>
   );
 }
