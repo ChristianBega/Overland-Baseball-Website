613b4a0067798c41336da075bbf55ec511373000
Chris <cbega1227@gmail.com>
2023-09-18 22:39:47 -0600

    Created start of booster page, still need to add forms and links

--- client/src/components/reusableComponents/textComponent.component.jsx
+++ client/src/components/reusableComponents/textComponent.component.jsx
@@ -1,23 +1,15 @@
 import { Grid, Typography } from "@mui/material";
 import React from "react";
 
-export default function TextComponent({ boosterData, eventsData, location }) {
+export default function TextComponent({ boosterData, isMobile }) {
   return (
     <>
-      {location === "boosters" && (
-        <Grid xs={12} item>
-          <Typography sx={{ backgroundColor: "lightcyan", height: "275px", p : 4 }} typography={{ xs: "smallBodyText", md : "largeBodyText" }}>
-            {boosterData.content}
-          </Typography>
-        </Grid>
-      )}
+      <Grid item xs={12} md={8} sx={{ padding: { xs: 0, md: 8 } }}>
+        {!isMobile && <Typography typography="h2">{boosterData.title}</Typography>}
+        <Typography sx={{ minHeight: "300px" }} typography={{ xs: "smallBodyText", md: "largeBodyText" }}>
+          {boosterData.content}
+        </Typography>
+      </Grid>
     </>
   );
 }
-
-// {location === "events" && (
-//   <Typography typography="smallBodyText">
-//     {/* Data is being passed into textComponent, but data isn't being displayed... do i need context for state???? */}
-//     {eventsData.content}
-//   </Typography>
-// )}
--- client/src/components/roster/staffItem.component.jsx
+++ client/src/components/roster/staffItem.component.jsx
@@ -4,17 +4,15 @@ export default function StaffItem({ currentRooster }) {
   return (
     <>
       {currentRooster.map((teamData) => (
-        <Box key={teamData.team} sx={{ display: "flex", justifyContent: "center", flexWrap: "wrap", gap: { xs: 5, md: 15 } }}>
+        <Box key={teamData.team} sx={{ display: "flex", justifyContent: "center", flexWrap: "wrap", gap: { xs: 5, md: 15 }, marginBottom: "5rem" }}>
           <Stack direction="row" spacing={4} sx={{ display: "flex", alignItems: "center" }}>
             <Box component="img" src={PlaceHolderImage} sx={{ width: "80px", height: "110px" }}></Box>
             <Stack direction="column" gap={3} sx={{ textAlign: "left", minWidth: "180px" }}>
               <Typography component="h2" variant="h6" fontWeight={500}>
                 Head Coach
               </Typography>
-              <Typography typography="bodyTextLg" sx={{ fontWeight: 700, fontSize: { md: "24px" } }}>
-                {teamData.coach}
-              </Typography>
-              <Typography>Contact</Typography>
+              <Typography typography="h3">{teamData.coach}</Typography>
+              {/* <Typography>Contact</Typography> */}
             </Stack>
           </Stack>
           <Stack direction="row" spacing={4} sx={{ display: "flex", alignItems: "center" }}>
@@ -23,15 +21,10 @@ export default function StaffItem({ currentRooster }) {
               <Typography component="h2" variant="h6" fontWeight={500}>
                 Assistant Coach
               </Typography>
-              <Typography typography="bodyTextLg" sx={{ fontWeight: 700, fontSize: { md: "24px" } }}>
-                {teamData.assistantCoach}
-              </Typography>
-              <Typography>Contact</Typography>
+              <Typography typography="h3">{teamData.assistantCoach}</Typography>
+              {/* <Typography>Contact</Typography> */}
             </Stack>
           </Stack>
-
-          {/* <Typography typography="bodyTextLg">{`Assistant Coach ${teamData.assistantCoach}`}</Typography>
-          <Typography typography="bodyTextLg">{`Team manager: ${teamData.teamManager}`}</Typography> */}
         </Box>
       ))}
     </>
