002f7f25260baf103ad81fb2786c426f69f45368
Chris <cbega1227@gmail.com>
2023-11-22 20:22:43 -0700

    Initialzed firebase app and updated .env file to include firebase app config.

---
+++ client/src/setup/utils/firebase/index.firebase.jsx
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// Import firebase firestore
+import { getFirestore } from "firebase/firestore";
+// Web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_API_KEY,
+  authDomain: process.env.REACT_AUTH_DOMAIN,
+  projectId: process.env.REACT_PROJECT_ID,
+  storageBucket: process.env.REACT_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+};
+
+// Initializing firebase instance
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Creating authentication and Firestore instances
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+
+export { firebaseApp, auth, db };
