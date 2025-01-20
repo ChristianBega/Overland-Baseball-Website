db122c1625cc311df0604c8863406c3ed0eee388
Chris <cbega1227@gmail.com>
2025-01-20 07:06:52 -0700

    Update the news section to include a new grid section for an image for screens sizes tablet and up.

--- client/src/pages/unauthorized/home/components/news/news.component.jsx
+++ client/src/pages/unauthorized/home/components/news/news.component.jsx
@@ -2,14 +2,17 @@
 import React from "react";
 import { useTheme } from "@emotion/react";
 // Mui
-import { Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 // Components
 import SectionLayout from "../../../../../components/reusableComponents/sectionLayout/sectionLayout.component";
 // Utils & Helpers
 import findKeyWordsAndHighlight from "../../../../../setup/utils/helpers/findKeyWordsAndHighLight";
+import useMediaQueries from "../../../../../setup/utils/helpers/useMediaQueries.utils";
+import TEMP_IMAGE from "../../../../../assets/placeholder.png";
 
 export default function News() {
   const theme = useTheme();
+  const { isMd } = useMediaQueries();
   // ! this data will be moved to the websiteData folder and mimic the cms structure we want. we will need to refactor later.
   const text = "Stay tuned for exciting news about our fundraisers, upcoming games, and all other events.";
   const keywords = [
@@ -23,22 +26,32 @@ export default function News() {
   const highlightedText = findKeyWordsAndHighlight(text, keywords, options);
 
   return (
-    <Grid item xs={12} md={12} lg={6} sx={{ position: "relative" }}>
-      <SectionLayout id="news-section" aria-label="News Section">
-        <Typography typography="h2" component="h2">
-          Blazer News
-        </Typography>
-        <Typography typography="p" component="p">
-          Welcome to the official site of Overland Baseball in Aurora, Colorado! At Overland, we’re dedicated to building strong, skilled players
-          through a focus on fundamentals, targeted training, and continuous improvement. Our Trailblazers Baseball program is rapidly growing and is
-          designed to support athletes ages 13-18 who are eager to elevate their game.
-        </Typography>
-        <Typography component="p">{highlightedText}</Typography>
-      </SectionLayout>
-
-      {/* <StyledSocialBox>
-          <Socials />
-        </StyledSocialBox> */}
-    </Grid>
+    <>
+      <Grid item xs={12} md={7} lg={6} sx={{ position: "relative" }}>
+        <SectionLayout id="news-section" aria-label="News Section">
+          <Typography typography="h2" component="h2">
+            Blazer News
+          </Typography>
+          <Typography typography="p" component="p">
+            Welcome to the official site of Overland Baseball in Aurora, Colorado! At Overland, we’re dedicated to building strong, skilled players
+            through a focus on fundamentals, targeted training, and continuous improvement. Our Trailblazers Baseball program is rapidly growing and
+            is designed to support athletes ages 13-18 who are eager to elevate their game.
+          </Typography>
+          <Typography component="p">{highlightedText}</Typography>
+        </SectionLayout>
+      </Grid>
+      {isMd && (
+        <Grid item xs={12} md={5} lg={6}>
+          <SectionLayout id="news-section-image-slider" aria-label="News Section Image Slider">
+            <Box component="img" src={TEMP_IMAGE} sx={{ border: "1px solid red", width: "100%", maxHeight: "350px" }}></Box>
+          </SectionLayout>
+        </Grid>
+      )}
+    </>
   );
+}
+{
+  /* <StyledSocialBox>
+          <Socials />
+        </StyledSocialBox> */
 }
--- client/src/pages/unauthorized/home/home.page.jsx
+++ client/src/pages/unauthorized/home/home.page.jsx
@@ -7,13 +7,15 @@ import Events from "./components/events/events.components";
 import Schedule from "./components/schedule/schedule.component";
 import ContactUs from "./components/contactUs/contactUs.component";
 import Sponsors from "../../../components/sponsors/sponsors.component";
+import useMediaQueries from "../../../setup/utils/helpers/useMediaQueries.utils";
 
 const HomePage = () => {
+  const { isLg } = useMediaQueries();
   return (
     <>
       <Hero />
       <Container component="main" id="home-page" aria-label="Home Page">
-        <Grid container id="home-page-grid">
+        <Grid container id="home-page-grid" columnSpacing={isLg ? 6 : 4}>
           <News />
           <Events />
           <Schedule />
