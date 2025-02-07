f2a7b086e6cb4f82f000423745db3a8d8ebcdd47
Chris <cbega1227@gmail.com>
2025-02-07 07:44:46 -0700

    Started working on refactoring the events page and the Events component. Integrated a proof of concept embedded google calendar to replace the previous events table grid.

--- client/src/pages/unauthorized/events/events.page.jsx
+++ client/src/pages/unauthorized/events/events.page.jsx
@@ -12,23 +12,29 @@ import YouthProgram from "./components/youthProgram/youthProgram.component";
 import YouthProgramSlider from "./components/youthProgramSlider/youthProgramSlider.component";
 import Workouts from "./components/workouts/workouts.component";
 import WorkoutSlider from "./components/workoutSlider/workoutSlider.component";
+// Utils & Helpers
+import useMediaQueries from "../../../setup/utils/helpers/useMediaQueries.utils";
 
 export default function EventsPage() {
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const { isLg, isMd } = useMediaQueries();
+  // const isMobile = useMediaQuery(theme.breakpoints.down("md"));
   return (
     <Container
-      component={motion.section}
-      initial={containerVariants.hidden}
-      animate={containerVariants.visible}
-      exit={containerVariants.exit}
-      transition={containerVariants.transition}
-      id="events-section"
-      sx={{ display: "flex", justifyContent: " center" }}
+      // component={motion.section}
+      // initial={containerVariants.hidden}
+      // animate={containerVariants.visible}
+      // exit={containerVariants.exit}
+      // transition={containerVariants.transition}
+      id="events-page"
+      component="main"
+      aria-label="Events Page"
     >
-      <Grid id="events-main-grid" container maxWidth="lg" rowSpacing={isMobile ? 12 : 32}>
+      {/* maxWidth="lg" rowSpacing={isMobile ? 12 : 32} */}
+
+      <Grid container id="events-main-grid" columnSpacing={isLg ? 6 : 4}>
         <Events />
-        {isMobile && <YouthProgramSlider isMobile={isMobile} />}
+        {/* {isMobile && <YouthProgramSlider isMobile={isMobile} />}
         <YouthProgram isMobile={isMobile} />
         {!isMobile && <YouthProgramSlider />}
         <TryoutsSlider isMobile={isMobile} />
@@ -36,7 +42,7 @@ export default function EventsPage() {
         {isMobile && <WorkoutSlider isMobile={isMobile} />}
         <Workouts isMobile={isMobile} />
         {!isMobile && <WorkoutSlider />}
-        <Fundraisers />
+        <Fundraisers /> */}
       </Grid>
     </Container>
   );
