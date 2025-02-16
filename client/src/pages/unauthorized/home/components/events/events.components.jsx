d1b663d2f4fb5f11e6f6b56accd1e81479a97168
Chris <cbega1227@gmail.com>
2025-02-16 08:42:26 -0700

    Updated the events component on the home page to use the realtime data and the new bento layout. Also removed any old code from this file.

--- client/src/pages/unauthorized/home/components/events/events.components.jsx
+++ client/src/pages/unauthorized/home/components/events/events.components.jsx
@@ -4,40 +4,20 @@ import { useTheme } from "@emotion/react";
 import { Grid, Typography } from "@mui/material";
 //  Components
 import SectionLayout from "../../../../../components/reusableComponents/sectionLayout/sectionLayout.component";
+import BentoLayout from "../../../../../components/reusableComponents/bentoLayout/bentoLayout.component";
 // Utilities
 import findKeyWordsAndHighlight from "../../../../../setup/utils/helpers/findKeyWordsAndHighLight";
-// Data
-import cardMockData from "../../../../../websiteData/events/events.data.json";
-// Assets
-import cardImgOne from "../../../../../assets/homePage/events/card-img-1.svg";
-import cardImgTwo from "../../../../../assets/homePage/events/card-img-2.svg";
-import cardImgThree from "../../../../../assets/homePage/events/card-img-3.svg";
-import cardImgFour from "../../../../../assets/homePage/events/card-img-4.svg";
-
+import { useRealtimeData } from "../../../../../hooks/useRealtimeData";
 import useMediaQueries from "../../../../../setup/utils/helpers/useMediaQueries.utils";
-import BentoLayout from "../../../../../components/reusableComponents/bentoLayout/bentoLayout.component";
-
-// ! lines 20-31 are temp for mockData... Once we move to DB eventImageUrl will be required instead of the temp image solution below
-const images = {
-  cardImgOne,
-  cardImgTwo,
-  cardImgThree,
-  cardImgFour,
-};
-
-const events = cardMockData.map((event) => ({
-  ...event,
-  image: images[event.image] || event.image,
-}));
-// ! lines 20-31 are temp for mockData... Once we move to DB eventImageUrl will be required instead of the temp image solution below
 
 const Events = () => {
   const { isMd } = useMediaQueries();
   const theme = useTheme();
+  const { data, isLoading, error } = useRealtimeData("events");
+  const featuredEvents = data?.filter((event) => event.eventType === "featured");
 
-  // const { data, isLoading, error } = useRealtimeData("events");
-  // if (isLoading) return "loading...";
-  // if (error) return "error...";
+  if (isLoading) return "loading...";
+  if (error) return "error...";
 
   const eventInfoPageData = {
     text: "Help support your Overland Trailblazer baseball program by attending our upcoming events. We have lots happening in our community including fundraisers, team activities, and more. Check out all our events here and sign up!",
@@ -55,7 +35,7 @@ const Events = () => {
         <Typography component="p" typography="p" marginBottom={isMd ? 4 : 2}>
           {highlightedText}
         </Typography>
-        {/* <BentoLayout gridItemsData={events} /> */}
+        <BentoLayout gridItemsData={featuredEvents} />
       </SectionLayout>
     </Grid>
   );
